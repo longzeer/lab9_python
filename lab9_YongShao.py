385c28fb4c3d91a32bdd36576a52cd1ad3bc4f6e
longzeer <shao0015@algonquinlive.com>
2019-11-11 12:33:07 -0500

    fixing error: string out of range

--- lab9_YongShao.py
+++ lab9_YongShao.py
@@ -43,6 +43,7 @@ f.close()
 
 
 #Task3
+
 def generateDictionary(mydict={},oldfin = open('font3.txt','r+')): 
     for row in oldfin:
         key=row[-2]
@@ -54,6 +55,10 @@ def generateDictionary(mydict={},oldfin = open('font3.txt','r+')):
 def clearScreen(lcd):
     lcd.clear()
     lcd.show()
+
+def clearBacklight():
+    backlight.set_all(0,0,0)
+    backlight.show()
 
 def displayObject(obj,x,y):
     i=0
@@ -89,20 +94,31 @@ if nowdict.__contains__(userCharacter) == True:
     i=0
     mylist=[]
     for n in range(0,int(len(objectmom)/2)+1):
-        str1 = objectmom[i:i+2]                         #divide 8 charactors to 4 2-charactor
-        #print(str1[0])
-        Obbin1=bin(int(str1[0],16))                 #conver first hex number to bin
-        bin1=Obbin1[2:]                             #remove '0b'
-        Obbin2=bin(int(str1[1],16))                 #conver second hex number to bin
-        bin2=Obbin2[2:]
-        trans8to2='0'*(4-len(bin1))+bin1+'0'*(4-len(bin2))+bin2    #add proper '0' making the number 4-digit
-        s2l=list(trans8to2)  #convert string to int, then to list
-        listSToListI=[int(m) for m in s2l ]
-        mylist.append(listSToListI)
-        #print(mylist)
-        i+=2
-    clearScreen(lcd)
+        if i+2<=int(len(objectmom)):
+            str1 = objectmom[i:i+2]                         #divide 8 charactors to 4 2-charactor
+            #print(i)  for testing
+            Obbin1=bin(int(str1[0],16))                 #conver first hex number to bin
+            bin1=Obbin1[2:]                             #remove '0b'
+            Obbin2=bin(int(str1[1],16))                 #conver second hex number to bin
+            bin2=Obbin2[2:]
+            trans8to2='0'*(4-len(bin1))+bin1+'0'*(4-len(bin2))+bin2    #add proper '0' making the number 4-digit
+            s2l=list(trans8to2)  #convert string to int, then to list
+            listSToListI=[int(m) for m in s2l ]
+            mylist.append(listSToListI)
+            
+            i+=2
+    #print(mylist)     for testing
+    lcd.clear()
+    backlight.set_all(255,0,255)
+    backlight.show()
     displayObject(mylist,60,28)
+    time.sleep(3)
+    clearBacklight()
 else:
-    displayText('It is not in my dictionary',lcd,5,12)
+    lcd.clear()
+    backlight.set_all(255,0,0)
+    backlight.show()
+    displayText('Not in dictionary',lcd,5,12)
+    time.sleep(3)
+    clearBacklight()
     
